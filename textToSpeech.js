744eb4b1b4681e8f82cfebcc3cae7dd3e15735ab
Hutking <69020648+Hutking@users.noreply.github.com>
2021-10-11 14:22:19 -0300

    Update textToSpeech.js

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,5 +1,5 @@
 let speech = new SpeechSynthesisUtterance();
-speech.lang = "en";
+speech.lang = "es";
 
 let voices = [];
 window.speechSynthesis.onvoiceschanged = () => {
@@ -9,22 +9,10 @@ window.speechSynthesis.onvoiceschanged = () => {
   voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
-document.querySelector("#rate").addEventListener("input", () => {
-  const rate = document.querySelector("#rate").value;
-  speech.rate = rate;
-  document.querySelector("#rate-label").innerHTML = rate;
-});
-
 document.querySelector("#volume").addEventListener("input", () => {
   const volume = document.querySelector("#volume").value;
   speech.volume = volume;
   document.querySelector("#volume-label").innerHTML = volume;
-});
-
-document.querySelector("#pitch").addEventListener("input", () => {
-  const pitch = document.querySelector("#pitch").value;
-  speech.pitch = pitch;
-  document.querySelector("#pitch-label").innerHTML = pitch;
 });
 
 document.querySelector("#voices").addEventListener("change", () => {
@@ -38,12 +26,4 @@ document.querySelector("#start").addEventListener("click", () => {
 
 document.querySelector("#pause").addEventListener("click", () => {
   window.speechSynthesis.pause();
-});
-
-document.querySelector("#resume").addEventListener("click", () => {
-  window.speechSynthesis.resume();
-});
-
-document.querySelector("#cancel").addEventListener("click", () => {
-  window.speechSynthesis.cancel();
 });
